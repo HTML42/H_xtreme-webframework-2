373981af32ff9e0ebcc25f7f6d8f8b4ac59bfb77
Mark Paspirgilis <external_github@power-of-m.de>
2018-03-20 19:31:50 +0100

    .

--- -init/init_bootstrap.php
+++ -init/init_bootstrap.php
@@ -10,7 +10,7 @@ if (strstr(__FILE__, 'xtreme' . DIRECTORY_SEPARATOR . '-init' . DIRECTORY_SEPARA
 define('APP_DIR', $app_dir);
 
 //Init-Functions
-function cp_r($target, $destination) {
+function cp_r($target, $destination, $force = false) {
     if (is_dir($destination) && substr($destination, -1) != '/') {
         $destination .= '/';
     }
@@ -26,14 +26,14 @@ function cp_r($target, $destination) {
         }
         foreach (scandir($target) as $folder_item) {
             if ($folder_item != '.' && $folder_item != '..') {
-                cp_r($target . $folder_item, $destination . $folder_item);
+                cp_r($target . $folder_item, $destination . $folder_item, $force);
             }
         }
     } else if (is_file($target)) {
         if (!is_dir($destination)) {
             @mkdir($destination);
         }
-        if (!is_file($destination)) {
+        if (!is_file($destination) || $force) {
             copy($target, $destination);
         }
     }
