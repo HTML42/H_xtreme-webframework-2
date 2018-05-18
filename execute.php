fec0f7cfc007f744e03b2fc7cbf531045223c0ad
Mark Paspirgilis <external_github@power-of-m.de>
2018-05-18 09:55:01 +0200

    gpdr

--- execute.php
+++ execute.php
@@ -4,7 +4,9 @@ ini_set('short_open_tag', 1);
 ini_set('magic_quotes_gpc', 1);
 ini_set("memory_limit", "512M");
 //
-
+if(!getenv('NOGPDR')) {
+    include 'library/xgpdr.php';
+}
 include 'library/bootstrap.php';
 
 include LIB . 'handle_file.php';
