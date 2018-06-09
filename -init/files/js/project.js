54fe2f2cee0f16f195d8c34bd89ce7a3c726d5c3
Mark Paspirgilis <mark-paspirgilis@web.de>
2018-06-09 13:11:39 +0200

    .

--- -init/files/js/project.js
+++ -init/files/js/project.js
@@ -1,7 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+var Xtreme_startup_calls = Xtreme_startup_calls || [];
+Xtreme_startup_calls.push(function() {
+    //
+    list_length();
+    //
+    $('#navi_toggler').click(function() {
+        $('#navigation ul').toggleClass('active');
+    });
+});
